b76855c62bc806ef88f03e659b4220380167dd2b
RaphaMitter <raphamitter27@gmail.com>
2024-05-22 22:10:24 -0300

    bind dos dados da listagem de contatos

--- bd/contatos.sql
+++ bd/contatos.sql
@@ -11,3 +11,11 @@ CREATE TABLE IF NOT EXISTS contatos_info (
     telefone VARCHAR(20),
     email VARCHAR(100) NOT NULL
 );
+
+SELECT * FROM contatos.contatos_info;
+
+INSERT INTO contatos_info (nome, telefone, email) VALUES 
+('João', '123456789', 'joao@mail.com'),
+('Maria', '987654321', 'maria@mail.com'),
+('Carlos', '999999999', 'carlos@mail.com'),
+('Ana', '888888888', 'ana@mail.com');
