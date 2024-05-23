7031e5a90eefbc057b051c8d148af9998b02df9d
RaphaMitter <raphamitter27@gmail.com>
2024-05-23 20:19:48 -0300

    correcao da navegacao das paginas

--- Database.php
+++ Database.php
@@ -1,42 +1,34 @@
 <?php
-class Database 
+class Database
 {
+    private static $instance = null;
+    private $conn;
+    private $host = 'localhost';
+    private $db = 'contatos';
+    private $user = 'root';
+    private $pass = '';
 
-private static $instance = null;
-private $conn;
-private $host = 'localhost';
-private $db = 'contatos';
-private $user = 'root';
-private $pass = '';
-
-private function __constructor()
-{
-    try
+    private function __construct()
     {
-        $this->conn = new PDD("mysql:host=$this->host;dbname=$this->db", $this->user, $this->pass);
-        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPETION);
-    } catch (PDOException $e)
-    {
-        echo 'Connection failed: ' . $e->getMessage();
+        try {
+            $this->conn = new PDO("mysql:host=$this->host;dbname=$this->db", $this->user, $this->pass);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo 'Connection failed: ' . $e->getMessage();
+        }
     }
 
-}
-
-// Singleton Design Patterns
-// https://refactoring.guru/design-patterns/singleton
-
-public static function getInstance()
-{
-    if(!self::$instance)
+    // Singleton Design Pattern
+    // https://refactoring.guru/design-patterns/singleton
+    public static function getInstance()
     {
-        self::$instance = new Database();
+        if (!self::$instance) {
+            self::$instance = new Database();
+        }
+        return self::$instance;
     }
-    return self::$instance;
-}
 
-public function getConnection() {
-    return $this->conn;
+    public function getConnection() {
+        return $this->conn;
+    }
 }
-
-}
-?>
